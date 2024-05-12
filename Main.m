424fc3b481216db69e4d678de80e2ed8d55e2a84
Recea Alex <recea.alex@yahoo.com>
2024-05-12 21:12:31 +0300

    am adaugat in fisierul onlyDataWithInput si input pentru azimuth cat si pentru pitch, dar valorile le-am dat de la mine si nu stiu daca trebuie sa fie chiar diferite de 0 din prima. Am identificat si functia de azimuth cu armax si am 89% asemanare

--- Main.m
+++ Main.m
@@ -3,12 +3,30 @@
 %coloana 3: Azimuth21
 %coloana 4: Pitch
 %coloana 5: Pitch12
+%intrareAzimuth: 0.2
+%intrarePitch: 0.3
 
-time = tout(:,1);
-azimuth = tout (:,2);
+
+time = tout(:, 1);
+azimuth = tout (:, 2);
 azimuth21 = tout (:, 3);
-pitch = tout (:,4);
-pitch12 = tout (:,5);
+pitch = tout (:, 4);
+pitch12 = tout (:, 5);
+azimuth_input = tout (:, 6);
+pitch_input = tout (:, 7);
 
 
-plot(time, azimuth)
+% plot(time, u1)
+% plot(time, pitch)
+Te = time(2) - time(1);
+
+data_azimuth_id = iddata(azimuth, azimuth_input, Te);
+
+nA = 2;
+nB = 2;
+nC = 1;
+nD = 2 ;
+armax_model_azimuth = armax(data_azimuth_id, [nA, nB, nC, nD])
+
+figure, compare(armax_model_azimuth, data_azimuth_id)
+
