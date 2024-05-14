bb9a3488b17b1350a85696e90198d1c89962456a
Recea Alex <recea.alex@yahoo.com>
2024-05-14 08:42:59 +0300

    am identificat toate functiile decuplate si am plotat raspunsul la treapta

--- Main.m
+++ Main.m
@@ -6,7 +6,7 @@
 %intrareAzimuth: 0.2
 %intrarePitch: 0.3
 
-
+close all
 time = tout(:, 1);
 azimuth = tout (:, 2);
 azimuth21 = tout (:, 3);
@@ -21,12 +21,36 @@ pitch_input = tout (:, 7);
 Te = time(2) - time(1);
 
 data_azimuth_id = iddata(azimuth, azimuth_input, Te);
+data_pitch_id = iddata(pitch, pitch_input, Te);
+data_azimuth21_id = iddata(azimuth21, azimuth_input, Te);
+data_pitch12_id = iddata(pitch12, pitch_input, Te);
 
 nA = 2;
 nB = 2;
 nC = 1;
-nD = 2 ;
-armax_model_azimuth = armax(data_azimuth_id, [nA, nB, nC, nD])
+nD = 2;
 
-figure, compare(armax_model_azimuth, data_azimuth_id)
+armax_model_azimuth = armax(data_azimuth_id, [nA, nB, nC, nD]);
+H_Azimuth_11 = tf (armax_model_azimuth.B, armax_model_azimuth.A, Te)
+armax_model_pitch = armax(data_pitch_id, [nA, nB, nC, nD]);
+H_Pitch_22 = tf (armax_model_pitch.B, armax_model_pitch.A, Te)
+armax_model_azimuth21 = armax(data_azimuth21_id, [1 1 1 1]);
+H_Azimuth_21 = tf (armax_model_azimuth21.B, armax_model_azimuth21.A, Te)
+armax_model_pitch12 = armax(data_pitch12_id, [1 1 1 1]);
+H_Pitch_12 = tf (armax_model_pitch12.B, armax_model_pitch12.A, Te)
+
+figure
+subplot (221)
+step(H_Azimuth_11)
+title ("Step Response for Azimuth Input")
+subplot (222)
+step(H_Pitch_12)
+title ("Step Response for Azimuth Input")
+subplot (223)
+step(H_Azimuth_21)
+title ("Step Response for Pitch Input")
+subplot(224)
+step(H_Pitch_22)
+title ("Step Response for Pitch Input")
+% figure, compare(armax_model_azimuth, data_azimuth_id)
 
